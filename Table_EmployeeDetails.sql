6ca73fefa7a3702fa485dd72fab75aef6f568543
kavitamakariye <kmakariye24@gmail.com>
2019-03-15 15:14:25 +0530

    creating table EmployeeDetails

--- Table_EmployeeDetails.sql
+++ Table_EmployeeDetails.sql
@@ -8,4 +8,13 @@ USE EmployeeManagement;
 --  Table 'EmployeeManagement'.'EmployeeDetails'
 -- ---------------------------------------------
 
-CREATE TABLE IF NOT EXISTS EmployeeDetails(EmployeeID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, First_Name VARCHAR(10) NOT NULL, Last_Name VARCHAR(10) NOT NULL, Address VARCHAR(60) NOT NULL, Designation VARCHAR(15) NOT NULL, Role VARCHAR(10) NOT NULL, Experience INT NOT NULL, Project_Name VARCHAR(70) NOT NULL, Supervisor_Name VARCHAR(20) NOT NULL);
+CREATE TABLE IF NOT EXISTS EmployeeDetails(
+EmployeeID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+First_Name VARCHAR(10) NOT NULL, 
+Last_Name VARCHAR(10) NOT NULL, 
+Address VARCHAR(60) NOT NULL, 
+Designation VARCHAR(15) NOT NULL, 
+Role VARCHAR(10) NOT NULL, 
+Experience INT NOT NULL, 
+Project_Name VARCHAR(70) NOT NULL, 
+Supervisor_Name VARCHAR(20) NOT NULL);
