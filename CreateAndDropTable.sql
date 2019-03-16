f0e28856e7f52b5ac5701004edf632baeee3189e
kavitamakariye <kmakariye24@gmail.com>
2019-03-16 09:35:56 +0530

    creating and dropping table CompanyDetails

--- CreateAndDropTable.sql
+++ CreateAndDropTable.sql
@@ -8,7 +8,10 @@ USE EmployeeManagement;
 -- Create table 'CompanyDetails'
 -- ---------------------------------------------
 
-CREATE TABLE IF NOT EXITS CompanyDetails(Company_Id int(11),Departments VARCHAR(20));
+CREATE TABLE IF NOT EXITS CompanyDetails(
+Company_Id int(11),
+Departments VARCHAR(20)
+);
 
 -- ---------------------------------------------
 -- Drop table 'CompanyDetails'
