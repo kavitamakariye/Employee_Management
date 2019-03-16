f44db6d70710fa3d6678a8cb81909f7ec75eee43
kavitamakariye <kmakariye24@gmail.com>
2019-03-16 09:34:47 +0530

    changing data-types of columns through alter command

--- AlterDataTypes_EmployeeDetails.sql
+++ AlterDataTypes_EmployeeDetails.sql
@@ -8,5 +8,9 @@ USE EmployeeManagement;
 -- Alter data-types of columns of table EmployeeDetails
 -- -----------------------------------------------------
 
-ALTER TABLE EmployeeDetails MODIFY Supervisor_Name VARCHAR(15) NOT NULL;
-ALTER TABLE EmployeeDetails MODIFY Impermanent_Employee BOOLEAN NOT NULL;
+ALTER TABLE EmployeeDetails 
+MODIFY Supervisor_Name VARCHAR(15) NOT NULL;
+
+
+ALTER TABLE EmployeeDetails 
+MODIFY Impermanent_Employee BOOLEAN NOT NULL;
