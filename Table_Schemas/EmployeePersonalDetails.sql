6c1441649fe069465a783d00e21ef51b24322843
kavitamakariye <kmakariye24@gmail.com>
2019-03-21 11:36:22 +0530

    modified EmployeePersonalDetails table

--- Table_Schemas/EmployeePersonalDetails.sql
+++ Table_Schemas/EmployeePersonalDetails.sql
@@ -12,7 +12,7 @@ USE EmployeeManagement;
 CREATE TABLE IF NOT EXISTS EmployeePersonalDetails(
 EmployeeID INT, 
 Address VARCHAR(60) NOT NULL,
-PersonalEmailID VARCHAR(20) NOT NULL,
+PersonalEmailID VARCHAR(25) NOT NULL,
 AadharCard BIGINT NOT NULL,
 Pancard VARCHAR(15) NOT NULL,
 Passport VARCHAR(15) NOT NULL,
